76cab0a7531feb7f7c6ad1101b283f7d5f969f65
StephanieSpanjian <spanjian@stephaniespanjian.com>
2024-02-06 14:35:09 -0800

    removing some console logs and commented out code.

--- app/components/SearchResultsList.tsx
+++ app/components/SearchResultsList.tsx
@@ -4,25 +4,8 @@ const SearchResultsList = (props: { q: string }) => {
   const { q } = props
   const { planets, isError, isLoading } = useSearchPlanets({ q })
 
-  console.log(planets)
-  console.log(isError)
-  console.log(isLoading)
-  // return <h1>{planets.name}</h1>
-
-  // {
-  //   /* <div className="fixed top-0 left-0 right-0">Contacts</div> */
-  // }
-  // {
-  //   /* <div className="grid grid-cols-2 gap-4 place-content-center h-48 ...">
-  //   <div>01</div>
-  //   <div>02</div>
-  //   <div>03</div>
-  //   <div>04</div>
-  // </div> */
-  // }
-
   return isLoading || !planets ? (
-    <>Loading...</> // <Skeleton />
+    <>Loading...</> // Should be some kind of <Skeleton />
   ) : (
     <div className="grid grid-cols-1 gap-4">
       {planets.length > 0 ? (
